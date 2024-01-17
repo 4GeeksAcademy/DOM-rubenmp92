b2e70487bb8a864f3cfd47eb6936b9f4bd540fe7
rubenmp92 <97123438+rubenmp92@users.noreply.github.com>
2024-01-17 19:30:06 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -1,11 +1,4 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
