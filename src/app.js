1cbbe230b77bf33fb44ebf1e92cc1fa9f30fab40
rubenmp92 <97123438+rubenmp92@users.noreply.github.com>
2024-01-17 19:50:40 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
-/* eslint-disable */
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
+let extension = [".com", ".net", ".us", ".io"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < extension.length; l++) {
+        let domain = pronoun[i] + adj[j] + noun[k] + extension[l];
+        console.log(domain);
+      }
+    }
+  }
+}
